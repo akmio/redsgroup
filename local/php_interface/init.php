29aa9676d0c97a052eb17e96e31a17675cad27f0
i.akmaev <i.akmaev@webprofy.ru>
2022-02-07 00:00:02 +0300

    added viewed product in email

---
+++ local/php_interface/init.php
@@ -0,0 +1,56 @@
+<?
+use \Bitrix\Catalog;
+
+AddEventHandler("sale", "OnOrderNewSendEmail", "bxModifySaleMails");
+
+function bxModifySaleMails($orderID, &$eventName, &$arFields)
+{
+   //id текущего пользователя
+   $idUser = CSaleBasket::GetBasketUserID();
+
+   //Получаем id товаров заказа
+   $resOrder = CSaleBasket::GetList(
+      [],
+      ['ORDER_ID' => $orderID, 'MODULE' => 'catalog'], 
+      false, 
+      false, 
+      ['PRODUCT_ID']
+   );
+
+   while ($arOrder = $resOrder->Fetch()) {
+      $products[] = $arOrder['PRODUCT_ID'];
+
+      //получаем id элемента, по id торгового предложения
+      $resElem[] = CCatalogSku::GetProductInfo($arOrder['PRODUCT_ID']);
+   }
+
+   foreach ($resElem as  $elem) {
+      $arElem[$elem['ID']] = $elem['ID'];
+
+   }
+
+   //получаем id просмотренных товаров
+   $resViewedProducts = Catalog\CatalogViewedProductTable::getList([
+      // 'select' => ['PRODUCT_ID'],
+      'select' => ['ELEMENT_ID'],
+      'filter' => ['FUSER_ID' => $idUser, '!ELEMENT_ID'=> $arElem],
+      'order' => array('ID' => 'DESC'),
+      'limit' => 10
+   ]);
+   while ($arViewProduct = $resViewedProducts->fetch())
+   {
+      $arProduct[] = $arViewProduct['ELEMENT_ID'];
+   }
+
+   //Получаем ссылку и название просмотренных элементов
+   $resViewElem = CIBlockElement::GetList(
+      ['ID' => 'ASC'],
+      ['ID' => $arProduct],
+      false,
+      false,
+      ['NAME', 'DETAIL_PAGE_URL']
+   );
+   while ($arViewElem = $resViewElem->GetNext()) {
+      $arFields['VIEWED_PRODUCTS'] .= '<a href ="'.$_SERVER['SERVER_NAME'].$arViewElem['DETAIL_PAGE_URL'].'">'.$arViewElem['NAME'].'</a><br>';
+   }
+}
